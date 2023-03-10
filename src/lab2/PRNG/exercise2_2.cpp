8c93f725f5fcaf70a5fc4ba22a7e12c5de270123
Giac97 <giacomo.becatti@studenti.unimi.it>
2023-03-10 08:54:48 +0100

    Updated ex2.2.cpp (removed useless commented part)

--- src/lab2/PRNG/exercise2_2.cpp
+++ src/lab2/PRNG/exercise2_2.cpp
@@ -175,49 +175,8 @@ int main (int argc, char *argv[]){
    out_rw.col(0) = Avg_final;
    out_rw.col(1) = Err_final;
 
-   out_rw.save("Sussy.dat", raw_ascii);
-   /** 
-   for (int i = 0; i < N; i++)
-   {
-       double SUM = 0.;
-       for (int j = 0; j<L; j++)
-       {
-           int k = j + i * L;
-           SUM += r(k);
-       }
-       I_mc(i) = SUM / L;
-       I_mc2(i) = I_mc(i) * I_mc(i);
-   }
-*/
-/**    
-    
-   rowvec I_prog(N, fill::zeros);
-   rowvec I_prog2(N, fill::zeros);
-   rowvec I_err(N, fill::zeros); 
+   out_rw.save("rw_discrete_distance.dat", raw_ascii);
 
-   for (int i = 0; i<N; i++)
-   {
-       for (int j = 0; j <= i; j++)
-       {
-           I_prog(i) += I_mc(j);
-           I_prog2(i) += I_mc2(j);
-       }
-       I_prog(i) /= float(i + 1);
-       I_prog2(i) /= float(i + 1);
-       I_err(i) = error(I_prog, I_prog2, i);
-   }
-
-   colvec Icol = I_prog.st();
-   colvec errcol = I_err.st();
-   vec x = regspace(0, N - 1); 
-   
-   mat out(N,3, fill::zeros);
-   out.col(0) = x;
-   out.col(1) = Icol;
-   out.col(2) = errcol;
-
-	out.save("I_mc_uniform.dat", raw_ascii);
-*/
    rnd.SaveSeed();
    return 0;
 }
