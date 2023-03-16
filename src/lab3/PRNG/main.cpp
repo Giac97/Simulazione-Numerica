08ed905fa19bf0c8d640c36bc802bddde4dc9ddc
Giacomo <giacomo.becatti@studenti.unimi.it>
2023-03-16 14:46:17 +0100

    Added function for direct price at t

---
+++ src/lab3/PRNG/main.cpp
@@ -0,0 +1,75 @@
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "random.h"
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
+
+double integrand(x)
+{
+    return M_PI / 2. * cos( M_PI * x / 2.);
+}
+
+int main (int argc, char *argv[]){
+
+   Random rnd;
+   int seed[4];
+   int p1, p2;
+   ifstream Primes("Primes");
+   if (Primes.is_open()){
+      Primes >> p1 >> p2 ;
+   } else cerr << "PROBLEM: Unable to open Primes" << endl;
+   Primes.close();
+   ifstream input("seed.in");
+   string property;
+   if (input.is_open()){
+      while ( !input.eof() ){
+         input >> property;
+         if( property == "RANDOMSEED" ){
+            input >> seed[0] >> seed[1] >> seed[2] >> seed[3];
+            rnd.SetRandom(seed,p1,p2);
+         }
+      }
+      input.close();
+   } else cerr << "PROBLEM: Unable to open seed.in" << endl;
+   
+   //TOtal number of throws
+   int M = 10000;
+
+   //Number of blocks
+   int N = 100;
+
+   //Number of throws per block
+   int L = M / N;
+    
+   
+
+   
+
+
+
+   rnd.SaveSeed();
+   return 0;
+}
+
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
